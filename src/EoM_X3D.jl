ff2da0a648c88f85509c9f135b73e837064d279e
BPMinaker <bminaker@uwindsor.ca>
2017-09-28 16:10:03 -0400

    initial file list in module

--- src/EoM_X3D.jl
+++ src/EoM_X3D.jl
@@ -6,7 +6,18 @@ import EoM
 
 export animate_modes
 
-#include("write_report.jl")
+include("animate_modes_x3d.jl")
+include("eom_draw_x3d.jl")
+
+include("x3d_animate.jl")
+include("x3d_body.jl")
+include("x3d_connections.jl")
+include("x3d_cyl.jl")
+include("x3d_ifs.jl")
+include("x3d_motion.jl")
+include("x3d_pnt.jl")
+include("x3d_save.jl")
+
 #include(joinpath("tex","tex_bode_pgfplot.jl"))
 
 end
