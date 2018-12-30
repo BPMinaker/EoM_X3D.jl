cc981228cb1b6aa28fa2bb7950e2d3fea0048406
BPMinaker <bminaker@gmail.com>
2018-12-30 17:38:22 -0500

    small bell crank edit

--- src/x3d_body.jl
+++ src/x3d_body.jl
@@ -3,7 +3,7 @@ function x3d_body(syst)
 ## GPL here
 # This function builds an x3d model
 
-x3d_tags=["ground" "upright" "strut" "arm" "wheel" "chassis" "bell" "rack" "rod" "bar"]  ## Define significant body names
+x3d_tags=["ground" "upright" "strut" "arm" "wheel" "chassis" "rack" "crank" "rod" "bar"]  ## Define significant body names
 
 for i in syst.bodys  ## Loop over each body
 
@@ -25,13 +25,13 @@ for i in syst.bodys  ## Loop over each body
 				x3d*=x3d_cyl([[0,abs(lcn[3])/3.9,0] [0,-abs(lcn[3])/3.9,0]],rad=0.8*abs(lcn[3]),col=[0.25,0.25,0.25],tran=0.8)
 
 			elseif j==6 #"chassis"
-				x3d=x3d_pnt([0,0,0],cubes=true,rad=[0.8,0.3,0.5],col=[0,0,0.5],tran=0.3)
+				x3d=x3d_pnt([0,0,0],cubes=true,rad=[0.8,0.3,0.4],col=[0,0,0.5],tran=0.3)
 
-			elseif j==7 #"bell"
-				x3d=x3d_pnt([0,0,0],cubes=true,rad=[0.04 0.04 0.04],col=[0.3,0.3,0.6])
-
-			elseif j==8 #"rack"
+			elseif j==7 #"rack"
 				x3d=x3d_cyl([[0,-0.1,0] [0,0.1,0]],rad=0.05,col=rand(3,1))
+
+			elseif j==8 #"crank"
+				x3d=x3d_pnt([0,0,0],rad=0.02,col=[0.5,0,0])
 
 			elseif j==9 || j==10 #"rod" or "bar"
  				x3d=x3d_pnt([0,0,0],rad=0.015,col=[0.5,0,0])
