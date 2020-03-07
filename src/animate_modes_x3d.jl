a5a1a450d3832948304cfa99abd653ce6f778743
BPMinaker <bminaker@gmail.com>
2020-03-06 20:56:46 -0500

    added folder argument

--- src/animate_modes_x3d.jl
+++ src/animate_modes_x3d.jl
@@ -1,4 +1,4 @@
-function animate_modes(folder,mbd,result;verbose=false)
+function animate_modes(mbd,result;folder="output",verbose=false)
 ## Copyright (C) 2017, Bruce Minaker
 ## animate_modes.jl is free software; you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by
